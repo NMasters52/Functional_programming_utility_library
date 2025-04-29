df5023ec7f828512bf233e4117bd59f9d9b42a63
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-29 15:57:05 +0000

    add partial application

--- concepts/closures.js
+++ concepts/closures.js
@@ -8,10 +8,32 @@ function outerFunc(outer){
     }
 }
 
-const outerVar = outerFunc("Outer Func");
-const innerVar = outerVar('Inner');
+const outerVar = outerFunc("I am the outer Func");
+const innerVar = outerVar('I am the inner Func:');
 innerVar;
 
 //create currying
+function add(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+    }
+}
 
-//create partial application
+const firstNum = add(1);
+const secondNum = firstNum(2);
+const thirdNum = secondNum(3);
+console.log(thirdNum)
+
+//create partial application in es6
+const partialyApply = (funcName, ...someArgs) => {
+    return (...moreArgs) => {
+        return funcName(...someArgs, ...moreArgs);
+    }
+}
+
+let sum = (a, b ,c) => a + b + c;
+const add10 = partialyApply(sum, 10);
+const result = add10(12, 13);
+console.log(result);
